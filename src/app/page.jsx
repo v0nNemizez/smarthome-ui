8a26358254c4105008d635334b3a89257579ae6d
Stein Petter Tokvam <stto1210@gmail.com>
2024-12-19 15:30:24 +0100

    migrate to nextja

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Name from "../components/Name";
+import LiveMeasurement from "../components/LiveMeasurement";
+
+export default function Home() {
+  
+  return (
+  <div>
+    <Name />
+    <LiveMeasurement homeId="f8b55fa3-1bf4-440f-aa0f-39f8fd2a49ca" />
+  </div>
+);
+  
+}
